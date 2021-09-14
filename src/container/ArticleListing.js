06cc9b93dfd6b68f355afce2ae7621de00208092
MyMac <mymac@10-18-23-55.dynapool.wireless.nyu.edu>
2021-09-14 08:45:48 -0400

    create container

---
+++ src/container/ArticleListing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ArticleListing() {
+    const articlesTitle = "Articles"
+    return (
+        <div>
+            <h1>{articleTitle}</h1>
+        </div>
+    )
+}
+
+export default ArticleListing;
