06398c2cf6450796130d30138b3510e262d889f3
MyMac <mymac@10-18-23-55.dynapool.wireless.nyu.edu>
2021-09-16 09:53:15 -0400

    styled articles

--- src/container/ArticleListing.js
+++ src/container/ArticleListing.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import ArticleCard from "../components/ArticleCard";
+import Data from "../components/data.json";
 
 function ArticleListing() {
-    const articleTitle = "Articles"
+    console.log(Data);
+    const articleTitle = `Articles`;
     return (
         <div className = "PageWrapper">
-            <h1>{articleTitle}</h1>
+            <h1 className = "HeaderOne">{articleTitle}</h1>
+            {Data.map((article,key) => (
+                <ArticleCard article = {article} key = {key} />
+            ))}
+            
         </div>
-    )
+    );
 }
 
 export default ArticleListing;
